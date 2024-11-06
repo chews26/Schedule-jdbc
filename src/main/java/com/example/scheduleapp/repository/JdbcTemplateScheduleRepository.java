3c040a46e8707a20d23f5faa6864a4de33b1687b
shine26 <chews26@naver.com>
2024-11-06 19:04:39 +0900

    :wrench: 생성 : DB연결 및 기본설정

--- src/main/java/com/example/scheduleapp/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/example/scheduleapp/repository/JdbcTemplateScheduleRepository.java
@@ -1,4 +1,14 @@
 package com.example.scheduleapp.repository;
 
-public class JdbcTemplateScheduleRepository {
+import com.example.scheduleapp.dto.ScheduleResponseDto;
+import com.example.scheduleapp.entity.Schedule;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcTemplateScheduleRepository implements ScheduleRepository {
+
+    @Override
+    public ScheduleResponseDto saveSchedule(Schedule schedule) {
+        return null;
+    }
 }
